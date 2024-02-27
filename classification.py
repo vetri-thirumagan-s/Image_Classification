a0833c4a09f01dcd61bd81b4972bade174f2822b
VETRI THIRUMAGAN S <124275413+vetri-thirumagan-s@users.noreply.github.com>
2024-02-28 02:07:38 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,14 +1,12 @@
 #Importing Packages
 from ultralytics import YOLO
+from PIL import Image
 
 # Load a model
 model = YOLO('yolov8x.pt')  # pretrained YOLOv8x model
 
-#input list of Images
-src = ["C:\\Users\\vetri\\OneDrive\\Pictures\\Download\\cat.jpeg"]
-
 def classify(image):
-
+    image = Image.open(image)
     # Run batched inference on a list of images
     results = model(image) 
 
@@ -18,9 +16,4 @@ def classify(image):
         masks = result.masks  # Masks object for segmentation masks outputs
         keypoints = result.keypoints  # Keypoints object for pose outputs
         probs = result.probs  # Probs object for classification outputs
-        # result.show()  # display to screen
         result.save(filename='result.jpg')  # save to disk
-        # result.show()
-
-classify('dog.jpeg')
-    
