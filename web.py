f58a39686945144fe923320a272f4f58e9381e6f
VETRI THIRUMAGAN S <124275413+vetri-thirumagan-s@users.noreply.github.com>
2024-02-28 02:08:27 +0530

    Update web.py

--- web.py
+++ web.py
@@ -9,6 +9,7 @@ st.title("Image Classification for Object Recognition")
 input_src = st.file_uploader("upload here", type=['png', 'jpeg', 'jpg'])
 
 st.image(input_src)
+st.write("Input Image")
 img = classify(input_src)
 
-st.image(Image('result.jpg'))
+st.image(Image.open('result.jpg'))
